6b166065b5485f85f11b666ae8357e94d5e95918
kpomier777 <kevinpomier.kp@gmail.com>
2022-09-14 10:12:52 -0400

    task3 basic emxample form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'hola ';
 
-    onSubmit(form: any) {
-        console.log("Form " ,form)
+
+  public colorControl = new FormControl ('');
+
+
+
+    constructor() {
     }
+
+    ngOnInit() {
+        this.colorControl.valueChanges.subscribe( value => {
+            console.log('values changes : ' , value)
+        })
+    }
+
+    onSubmit(form: any) {
+        console.log("Form " ,form
+        )
+    }
+
+    printColorControl(){
+      console.log(this.colorControl)
+    }
+
+
 }
