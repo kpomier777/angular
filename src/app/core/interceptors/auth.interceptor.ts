be1e07fd207b0e6297d32a899853656277ac95f2
kpomier777 <kevinpomier.kp@gmail.com>
2022-09-14 10:56:40 -0400

    09-9-2022 task1 auth interceptor part1

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,6 +1,14 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpParams, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
+
+import {
+    HttpResponse,
+    HttpEvent,
+    HttpHandler,
+    HttpInterceptor,
+    HttpParams,
+    HttpRequest, HttpErrorResponse
+} from "@angular/common/http";
+import {catchError, Observable, throwError} from "rxjs";
 import {AuthService} from "../../pages/services/auth.service";
 
 @Injectable()
@@ -21,7 +29,19 @@ export class AuthInterceptor implements HttpInterceptor {
             })
         }
 
-        return next.handle(request);
+        return next.handle(request).pipe(
+            catchError((err: HttpErrorResponse) => {
+                if (err.status === 401) {
+                    this.handler401Error();
+                }
+                return throwError('ERROR')
+            })
+        );
+    }
+
+    private handler401Error() {
+        this.authService.logout();
+        return throwError('ERROR 401');
 
     }
 
