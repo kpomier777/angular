93a31b4d48389ed23565f3cde29c084e7c72ba08
kpomier777 <kevinpomier.kp@gmail.com>
2022-09-14 08:42:31 -0400

    tracking branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,42 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
-
-
-  constructor() {
-    // Operator Splice
-    const A = ['a','b','c','d','e'];
-    const auxSplice = A.splice(1,1)
-    console.log('auxSplice:', A, auxSplice)
-
-    // Operador Reduce
-    const auxReduce = [1,2,3,4,5].reduce(
-        (prev, value, index)=> prev + value, 0
-    )
-    console.log("auxReduce: ", auxReduce)
-
-    // JSON Operators
-    const auxJSON = {
-      id: 1,
-      name: 'JUAN',
-      lasName: 'GUTIERREZ'
-    };
-
-    // ENTRIES, KEY, VALUES
-
-    const auxentries = Object.entries(auxJSON)
-    console.log("auxentries:", auxentries)
-
-    const auxKeys = Object.keys(auxJSON)
-    console.log("auxKeys:", auxKeys)
-
-    const auxValues = Object.values(auxJSON)
-    console.log("auxValues:", auxValues)
-
-
-  }
-
-  getRest(...params: any[]){
-    console.log('REST' , params)
-  }
 }
