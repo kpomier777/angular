acea96f386822d321f6de2caf3727dc4c283ca8e
kpomier777 <kevinpomier.kp@gmail.com>
2022-09-15 01:38:48 -0400

    task1 observable

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'hola ';
+
+  public data$ = of('hola');
+
+  constructor() {
+    this.data$.pipe(map((d) => d + '-map')).subscribe((res) => {
+      console.log('soy suscriptor:', res);
+    });
+  }
+}
