c870c660d5ec52d0bc1bd93f560fcf3e08235b64
kpomier777 <kevinpomier.kp@gmail.com>
2022-09-15 00:31:25 -0400

    arrow function

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  public title = 'test';
+
+  constructor() {
+    console.log('hi -  hola ');
+  }
+
+  private getName(name: string): string {
+    return 'hi' + name;
+  }
+
+  // --- Arrow function
+  // const getName = (name: string) => {return "hi" + name;}
+  // const getName = (name: string) => "hi" + name
+  // const getName = () => "hi"
+  // const getName = (name:string, lasName:string) => lasName
+
+  // const mult = num => num*2
+  private mult(num: any) {
+    return num;
+  }
+}
