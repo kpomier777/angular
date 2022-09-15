9da775aef8f050f7afa4bcb6531a1c42c8622f83
kpomier777 <kevinpomier.kp@gmail.com>
2022-09-15 01:24:02 -0400

    task3 do check

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,5 +1,6 @@
 import {
   Component,
+  DoCheck,
   Input,
   OnChanges,
   OnInit,
@@ -11,12 +12,20 @@ import {
   templateUrl: './test1.component.html',
   styleUrls: ['./test1.component.css'],
 })
-export class Test1Component implements OnInit, OnChanges {
+export class Test1Component implements OnInit, OnChanges, DoCheck {
   @Input() name!: string;
   @Input() lastName: string = 'perez';
 
+  public middleName: string = '';
+  public generalData: string = '';
+
   constructor() {
     console.log('constructor');
+  }
+
+  ngDoCheck(): void {
+    console.log('Do Check');
+    this.generalData = this.name + ' ' + this.lastName + '-' + this.middleName;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
